ddc996e747df7e4747df7d839607d39b99b8c266
Sanskriti <sanskriticool.23@gmail.com>
2020-07-12 21:01:39 +0530

    update

--- new.js
+++ new.js
@@ -5,66 +5,70 @@ var count;
 var winner;
 var button;
 var playerinfo;
-function setup() {
-	sz = 600;
+var playercount=1;
+var input;
+var greeting;
+var imageclicked;
+var color;
+var playerno;
+function setup() 
+{
+	sz =windowWidth*0.4;
 	sc = sz/3;
-	createCanvas(sz,sz);
-	reset();			// Set state of board = new game
-	// button = createButton('reset',5);
-	// button.position(220, 620);
-	// button.size(170,100);
-	// button.style('font-size','300%');
-	// button.mouseReleased(reset);
-	playerinfo = createP();
-	playerinfo.position(650,20);
-	playerinfo.style('font-size','200%');
-	
-
+	createCanvas(windowWidth,windowHeight);
+	reset();			
 }
-function reset(){
+
+function reset()
+{
     arr = [1,2,3,4,5,6,7,8,9];
     count = 0;
     player1turn = true;
     gameover = false;
+    playercount=1;
+    document.getElementById("change1").innerHTML = '';
+    document.getElementById("change2").innerHTML = '';
+    document.getElementById("change3").innerHTML = '';
 }
+
 function draw() {
     background(251);
-    for(i = 0;i < 9; i++){
-        fill(65,105,225);
-        strokeWeight(3);
-        stroke(255);
+    for(i = 0;i < 9; i++)
+    {
+        fill(51);
+        strokeWeight(5);
+        stroke('white');
         push();
         translate(((i%3)*sc),(floor((i/3))*sc));
         rect(0,0,sc,sc);
-        if(arr[i] == 0){				// Draw O
+        if(arr[i] == 0)
+        {                // Draw O
             ellipse(sc/2, sc/2, sc/2, sc/2);
         }
-        else if(arr[i] == -1){				// Draw X
+        else if(arr[i] == -1){              // Draw X
             line(sc/4, sc/4, 3 * (sc/4),3 * (sc/4));
             line(3 * (sc/4), (sc/4), (sc/4), 3 * (sc/4));
         }
         pop();
-        playerinfo.html("Player "+ ( (player1turn)?"1 (X) ":"2 (O) " )+ " turn"); // Update player info
     }
-    if(gameover){			// If game over
-        if(count===9){			// Check if draw, then print 'DRAW'
+    if(gameover)
+    {                           // If game over
+        if(count===9){          // Check if draw, then print 'DRAW'
             textAlign(CENTER, CENTER);
             textSize(64);
             fill(255);
             stroke(51);
             strokeWeight(3);
             text("Draw!", width/2, height/2);
-            playerinfo.html("Draw");
             return;
         }
-	// Draw line joining the winning streak
+    // Draw line joining the winning streak
         x1 = (winner[0]%3)*sc + sc/2;
         y1 = (floor(winner[0]/3))*sc + sc/2;
         x2 = (winner[1]%3)*sc + sc/2;
         y2 = (floor(winner[1]/3))*sc + sc/2;
-        strokeWeight(20);
+        strokeWeight(10);
         line(x1, y1, x2, y2);
-        playerinfo.html("Player "+((arr[winner[0]] === -1)?"1 (X) ":"2 (O) ") + "Won!");	// Update player info
     }
 }
 function mouseReleased(){	// Take user input at a cell
@@ -81,11 +85,7 @@ function mouseReleased(){	// Take user input at a cell
     count++;
     checkwon();
 }
-function keyPressed(){
-    if(key === 'r'|| key === 'R'){
-		reset();
-	}
-}
+
 function checkwon(){				// Check if in current board state player has won
 	if(count === 9) {			// If 9 turns are over, and no one has won, game is draw
 		gameover= true;
@@ -105,4 +105,59 @@ function checkwon(){				// Check if in current board state player has won
 			gameover = true;
 			winner = [i,i+b+b];	// Store first and last coordinate of winning boxes
 		}
+}
+function windowResized() 
+{ 
+    resizeCanvas(windowWidth, windowHeight); 
+    setup();
+} 
+function myfunction(image)
+{var name;
+ if(image==3)
+    {
+    name='AI';
+    greet(name);
+    playercount++;
+    playerinfo='AI';}
+ if(playercount<=2)
+  {
+  name=prompt('Player '+playercount+': Enter Your Name');
+  if(name!=null)
+   {greet(name);}
+  else
+   {alert('Please enter your name!');}
+  }
+  else
+  {alert("Only 2 players are allowed !");}
+}
+
+function image1()
+{imageclicked=1;myfunction(1);}
+function image2()
+{imageclicked=2;myfunction(2);}
+function image3()
+{imageclicked=3;myfunction(3);}
+
+
+
+function greet(name) 
+{ 
+  if(imageclicked==1)
+  {document.getElementById("change1").innerHTML = '  Player '+ playercount+': '+ name;} 
+
+  else if(imageclicked==2)
+  {document.getElementById("change2").innerHTML = '  Player '+ playercount+': '+ name;} 
+
+  else if(imageclicked==3)
+  {document.getElementById("change3").innerHTML = '  Player '+ playercount+': AI';} 
+ 
+ if(playercount==2)
+   {alert('PLAY!');}
+   playercount++;
+   input.value('');
+}
+//fucntion to changeCHANGE backgroundColor
+function changecolor()
+{color=document.getElementById('color').value;
+document.getElementById('sidebar').style.backgroundColor=color;
 }
